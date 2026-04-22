bd558cb2d977c7e02aefd62173ad9f5302d3e249
Arseniy Kushnir <arseniykushnir2007@gmail.com>
2026-04-22 19:20:36 +0300

    Created schemas for register

--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -1,4 +1,19 @@
-from config.settings import Settings
+from fastapi import Depends
 
-def get_settings():
+from config.settings import Settings
+from notifications.emails import EmailSender
+
+
+def get_settings() -> Settings:
     return Settings()
+
+
+def get_email_sender(
+        settings: Settings = Depends(get_settings),
+) -> EmailSender:
+    return EmailSender(
+        smtp_server=settings.SMTP_SERVER,
+        smtp_port=settings.SMTP_PORT,
+        smtp_user=settings.SMTP_USER,
+        smtp_password=settings.SMTP_PASSWORD
+    )
--- src/notifications/emails.py
+++ src/notifications/emails.py
@@ -39,10 +39,8 @@ class EmailSender:
     async def send_activation_email(self, email: str, activation_link: str):
         ...
 
-
     async def send_activation_complete_email(self, email: str, login_link: str):
         ...
-
 
     async def send_password_reset_email(self, email: str, password_reset_link: str):
         ...
